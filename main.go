1b2b3309024c7f4f4d92d49913508dd05a2f4963
Devansh Mathur <devanshmathur@harness.io>
2024-09-17 15:19:06 +0530

    feat: [CI-14214]: Add PLUGIN_USER_ROLE_EXTERNAL_ID to CACHE main.go

--- main.go
+++ main.go
@@ -424,6 +424,11 @@ func main() {
 			Usage:  "external ID to use when assuming role",
 			EnvVars: []string{"PLUGIN_EXTERNAL_ID"},
 		},
+		&cli.StringFlag{
+			Name:   "user-role-external-id",
+			Usage:  "external ID to use when assuming secondary role",
+			EnvVars: []string{"PLUGIN_USER_ROLE_EXTERNAL_ID"},
+		},
 
 		// GCS specific Configs flags
 
@@ -637,6 +642,7 @@ func run(c *cli.Context) error {
 			UserRoleArn:           c.String("user-role-arn"),
 			OIDCTokenID:           c.String("oidc-token-id"),
 			ExternalID:            c.String("external-id"),
+			UserRoleExternalID:    c.String("user-role-external-id"),
 		},
 		Azure: azure.Config{
 			AccountName:    c.String("azure.account-name"),
