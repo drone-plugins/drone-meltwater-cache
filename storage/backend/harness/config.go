8a663b27d1bf85ea7756b95b3805c2d81b1d1c09
Devansh Mathur <devanshmathur@harness.io>
2025-04-03 02:17:35 +0530

    Adding ENV variables to main.go and making threshold and enable configurable.

--- storage/backend/harness/config.go
+++ storage/backend/harness/config.go
@@ -2,7 +2,11 @@ package harness
 
 // Config is a structure to store harness backend configuration.
 type Config struct {
-	AccountID     string
-	Token         string
-	ServerBaseURL string
+	AccountID              string
+	Token                  string
+	ServerBaseURL          string
+	MultipartChunkSize     int
+	MultipartMaxUploadSize int
+	MultipartThresholdSize int
+	MultipartEnabled       string
 }
