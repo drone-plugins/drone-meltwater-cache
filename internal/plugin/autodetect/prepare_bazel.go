f22bcb7aba3c8c5052b1a56503d70de2443388e0
Soumyajit Das <soumyajit.das@harness.io>
2026-02-19 18:03:06 +0530

    fix: [CI-20732]: fix bazel cache intel for bazel 9

--- internal/plugin/autodetect/prepare_bazel.go
+++ internal/plugin/autodetect/prepare_bazel.go
@@ -49,7 +49,10 @@ func appendToBazelrc(dir, pathToCache string) error {
 	// This is safe to cache/restore across builds (unlike output_user_root which includes install/)
 	// Note: We don't use --disk_cache because users may have remote build cache enabled,
 	// and disk_cache would be redundant (remote cache already stores compiled outputs)
-	cmdToOverrideRepo := "common --repository_cache=" + pathToCache + "\n"
+	// We disable --repo_contents_cache because in Bazel 9+ it defaults to inside repository_cache,
+	// and when that's inside the workspace, Bazel fails with "repo contents cache inside main repo" error
+	cmdToOverrideRepo := "common --repository_cache=" + pathToCache + "\n" +
+		"common --repo_contents_cache=\n"
 
 	if _, err := os.Stat(fileName); errors.Is(err, os.ErrNotExist) {
 		f, err := os.Create(fileName)
